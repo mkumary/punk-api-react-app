61a8fb4322a031a5cf3442d6abe42450934f283c
mkumary <manoj10690@gmail.com>
2018-10-20 23:10:48 +0530

    Updates

---
+++ precache-manifest.72b3fab2cf74635676a2cda5d5b6cb04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86da423485ea46168a4d",
+    "url": "/punk-api-react-app/static/js/runtime~main.86da4234.js"
+  },
+  {
+    "revision": "d30a1b0cfbabf0e5dfcf",
+    "url": "/punk-api-react-app/static/js/main.d30a1b0c.chunk.js"
+  },
+  {
+    "revision": "690392f5ffb84d66dba6",
+    "url": "/punk-api-react-app/static/js/1.690392f5.chunk.js"
+  },
+  {
+    "revision": "d30a1b0cfbabf0e5dfcf",
+    "url": "/punk-api-react-app/static/css/main.f522d337.chunk.css"
+  },
+  {
+    "revision": "ce814640be9d6b1dcc24ad7318c4c80f",
+    "url": "/punk-api-react-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.5acfa8bc5683a97d2a4d7d01a69c035a.js"
+  "/punk-api-react-app/precache-manifest.72b3fab2cf74635676a2cda5d5b6cb04.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myapp/index.html", {
+workbox.routing.registerNavigationRoute("/punk-api-react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
